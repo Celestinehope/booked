a8b701f2f0043b7d486156a684658b8d0dfa66ff
pjaaaay <boruettpj@gmail.com>
2023-06-22 04:45:33 +0300

    Forms

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,16 +13,18 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link href="{{asset('import/assets/css/style.css')}}" rel="stylesheet">
     </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+    <body class="font-sans text-White-900 antialiased" >
+        <div style="background-image: url({{asset('import/assets/img/29.jpg')}});" class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-green-100 dark:bg-brown-900">
+            <div >
+                <a href="/" style="font-type:bold;" class="logo" >
+                     BOOKED
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <div style="background-color:white;" class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white:bg-white shadow-md overflow-hidden sm:rounded-lg">
+            
                 {{ $slot }}
             </div>
         </div>
